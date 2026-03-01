bc8fc0c3d7e9ed267c9c877e2c644d0509c4dd74
Thanh Danh <thanhdanh431@gmail.com>
2026-03-01 17:35:31 +0700

    feat(service): implement IJwtService for token generation and management

---
+++ backend/Backend/Interfaces/IJwtService.cs
@@ -0,0 +1,12 @@
+﻿using Backend.Entities;
+
+namespace Backend.Interfaces
+{
+    public interface IJwtService
+    {
+        string GenerateToken(User user);
+        string GenerateRefreshToken();
+        string HashToken(string token);
+
+    }
+}
---
+++ backend/Backend/Services/JwtService.cs
@@ -0,0 +1,62 @@
+﻿using Backend.Configurations;
+using Backend.Entities;
+using Backend.Interfaces;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Backend.Services
+{
+    public class JwtService : IJwtService
+    {
+        private readonly JwtConfig _jwtConfig;
+
+        public JwtService(IOptions<JwtConfig> jwtConfig) => _jwtConfig = jwtConfig.Value;
+
+        public string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[64];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            }
+        }
+
+        public string GenerateToken(User user)
+        {
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtConfig.Secret));
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.Name)
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(_jwtConfig.AccessTokenExpirationMinutes),
+                Issuer = _jwtConfig.Issuer,
+                Audience = _jwtConfig.Audience,
+                SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(securityToken);
+        }
+
+        public string HashToken(string token)
+        {
+            using var sha256 = SHA256.Create();
+            var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(token));
+            return Convert.ToBase64String(hashedBytes);
+        }
+    }
+}
