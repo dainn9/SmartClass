b40b81846e1b176fec719780cd36723afa03eff9
Thanh Danh <thanhdanh431@gmail.com>
2026-03-01 17:32:47 +0700

    feat(configuration): add RefreshTokenExpirationDays property to JwtConfig

--- backend/Backend/Configurations/JwtConfig.cs
+++ backend/Backend/Configurations/JwtConfig.cs
@@ -6,5 +6,7 @@
         public string Issuer { get; set; } = string.Empty;
         public string Audience { get; set; } = string.Empty;
         public int AccessTokenExpirationMinutes { get; set; }
+        public int RefreshTokenExpirationDays { get; set; }
+
     }
 }
