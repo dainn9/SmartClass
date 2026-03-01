46d72b31ffaa17f5ccc903fcc799a75cbf9ad659
Thanh Danh <thanhdanh431@gmail.com>
2026-03-01 17:36:29 +0700

    feat(auth): implement AuthController for Google authentication

---
+++ backend/Backend/Controllers/AuthController.cs
@@ -0,0 +1,71 @@
+﻿using Backend.Common;
+using Backend.DTOs.Auth;
+using Backend.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login-google")]
+        public async Task<IActionResult> LoginWithGoogle([FromBody] GoogleLoginRequestDTO request)
+        {
+            var (dto, refreshToken) = await _authService.LoginWithGoogleAsync(request);
+
+            Response.Cookies.Append("refreshToken", refreshToken, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.None,
+                Expires = DateTimeOffset.UtcNow.AddDays(7)
+            });
+
+            return Ok(ApiResponse<GoogleLoginResponseDTO>.SuccessResponse(dto));
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _authService.LogoutAsync();
+            Response.Cookies.Delete("refreshToken", new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.None
+            });
+
+            return Ok(ApiResponse<object>.SuccessResponse());
+        }
+
+        [HttpPost("refresh")]
+        public async Task<IActionResult> Refresh()
+        {
+            var refreshToken = Request.Cookies["refreshToken"];
+
+            if (string.IsNullOrEmpty(refreshToken))
+                return Unauthorized();
+
+            var (refreshDto, newRefreshToken) = await _authService.RefreshTokenAsync(refreshToken);
+
+            Response.Cookies.Append("refreshToken", newRefreshToken, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.None,
+                Expires = DateTimeOffset.UtcNow.AddDays(7)
+            });
+
+            return Ok(ApiResponse<RefreshTokenResponseDTO>.SuccessResponse(refreshDto));
+        }
+
+    }
+}
